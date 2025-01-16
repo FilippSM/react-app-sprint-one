4a0ecfd56c339e272437d9353eccc7cfaa65a9a0
Filipp Sam <philippsm666@gmail.com>
2025-01-16 22:30:12 +0300

    (sprint 2 week 3) add useReducer

--- src/components/UncontrolledAccordionn/UncontrolledAccordionn.tsx
+++ src/components/UncontrolledAccordionn/UncontrolledAccordionn.tsx
@@ -1,18 +1,35 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 type AccordionPropsType = {
     titleValue: string;
 /*     collapsed: boolean; */
 }
 
+
+type ActionType = {
+    type: string
+}
+
+const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED";
+
+const reducer = (state: boolean, action: ActionType) => {
+    switch (action.type) {
+        case TOGGLE_CONSTANT:
+            return !state;
+        default:
+            throw new Error("Bad action type")
+    }
+}
 export function UncontrolledAccordion(props: AccordionPropsType) {
     console.log("rendiring Accordion");
 
-    let [collapsed, setCollapsed] = useState(false);
+    /* let [collapsed, setCollapsed] = useState(false); */
+    let [collapsed, dispath] = useReducer(reducer, false);
 
     return (
         <div>
-            <AccordionTitle title={props.titleValue} onClick={() => {setCollapsed(!collapsed)}}/>
+{/*             <AccordionTitle title={props.titleValue} onClick={() => {setCollapsed(!collapsed)}}/>
+ */}            <AccordionTitle title={props.titleValue} onClick={() => {dispath({type: TOGGLE_CONSTANT})}}/>
             {!collapsed && <AccordionBody />}
         </div>
     )
