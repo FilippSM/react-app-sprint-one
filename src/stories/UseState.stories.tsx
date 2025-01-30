0a381b6d4af0a819ec4622b98ba26f26130769df
Filipp Sam <philippsm666@gmail.com>
2025-01-30 23:18:28 +0300

    (sprint 3 week 3) useState

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -5,7 +5,7 @@ export default  {
 }
 
 function generateData() {
-    return 23231
+    return 1
 }
 
 export const Example1 = () => {
@@ -13,8 +13,12 @@ export const Example1 = () => {
 
     const [counter, setCounter] = useState(generateData);
 
+    const changer = (state: number) => {
+        return state + 1
+    }
+
     return <>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => setCounter(changer)}>+</button>
         {counter}
     </>
 }
