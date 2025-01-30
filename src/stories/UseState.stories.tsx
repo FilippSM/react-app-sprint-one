635db762a349a0de734e522282ef1bfdc2dc3025
Filipp Sam <philippsm666@gmail.com>
2025-01-30 23:00:13 +0300

    (sprint 3 week 3) useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,20 @@
+import React, { useMemo, useState } from "react"
+
+export default  {
+    title: 'useState demo'
+}
+
+function generateData() {
+    return 23231
+}
+
+export const Example1 = () => {
+    const initValue = useMemo(generateData, [])
+
+    const [counter, setCounter] = useState(initValue)
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        {counter}
+    </>
+}
